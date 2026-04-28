c51a539acb36f6183faf1eaffee8994fea5d1387
APPAS <appas.m@advertflair.studio>
2026-04-28 16:46:17 +0530

    fixed users urls properly

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,6 +3,6 @@ from .views import register_user, get_users, create_admin
 
 urlpatterns = [
     path('register/', register_user),
-    path('all/', get_users),
-    path('create-admin/', create_admin),  # ✅ REQUIRED
+    path('all/', get_users),                 # ✅ better than empty path
+    path('create-admin/', create_admin),     # ✅ THIS IS MISSING NOW
 ]
